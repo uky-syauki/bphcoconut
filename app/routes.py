028c239c178f893297094f7c75e801de23ce56ea
uky-syauki <achmad.uky@gmail.com>
2023-12-06 05:37:48 +0800

    versi 1.2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
+from flask import render_template, url_for
+
 from app import app
+
 
 @app.route("/")
 def index():
-	return "Hello Dunia"
+	return render_template("blank.html")
+
+
+@app.route("/login")
+def login():
+	return "Login View"
